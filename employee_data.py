5927f4b5d6dcf95493486fc29939fd010d632bba
haidersalim2060-spec <haider.salim2060@gmail.com>
2026-01-09 17:47:38 +0100

    Create employee_data.py

---
+++ employee_data.py
@@ -0,0 +1,16 @@
+def employee_data_checker(data):
+  employee_data = { "task_completed":0 , "hours_worked":0 , "quality_score":0 , "deadline_met":0 }
+  weights = { "task_completed":0 , "hours_worked":0 , "quality_score":0 , "deadline_met":0 }
+  total_score = 0
+for matric in employee_data:
+   total_score = total_score+(employee_data[metric] * weight[metric])
+if total_score<0.7:
+   employee_data "excellent"
+elif total_score<0.4:
+  employee_data "average"
+else:
+  employee_data "poor"
+
+return(rating)
+
+  
