5db669686801f7325988c74c9d258fd413145f54
haidersalim2060-spec <haider.salim2060@gmail.com>
2026-01-12 17:19:26 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+from employee_data import employee_data_checker   
+data = { "task_completed":0 , "hours_worked":0 , "quality_score":0 , "deadline_met":0 } 
+result = employee_data_checker(data)
+print(result)
